e29b30936935c7b05c64a8ef5ee6b72be43f1aaf
abdelrahmanmohh1 <abdelrahman.mohh1@gmail.com>
2026-05-05 07:26:57 +0300

    Add files via upload

---
+++ script-login.js
@@ -0,0 +1,32 @@
+function goToHome(e) {  e.preventDefault(); 
+ window.location.href = "index.html";
+}
+function toggleMode() {
+	document.body.classList.toggle("dark"); 
+	if (document.body.classList.contains("dark")) 
+	{    localStorage.setItem("mode", "dark");
+} 
+else {    localStorage.setItem("mode", "light"); 
+ }
+}
+window.onload = function () {  let mode = localStorage.getItem("mode");
+ if (mode === "dark")
+	 {    document.body.classList.add("dark");
+ }
+ };
+
+function logout() {
+    localStorage.removeItem("isLoggedIn");
+    window.location.href = "login.html";
+}
+function handleLogin(e) {
+    e.preventDefault(); // ashan el refreshing 
+    const userCode = document.getElementById("userCode").value;
+    const zipCode = document.getElementById("zipCode").value;
+    if (userCode !== "" && zipCode !== "") {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html";
+    } else {
+        alert("Please enter both the code and zip code!");
+    }
+}
