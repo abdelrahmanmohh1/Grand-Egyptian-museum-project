9dbb5cd7f05406c0ae0dff124a07944b85c6e49d
abdelrahmanmohh1 <abdelrahman.mohh1@gmail.com>
2026-05-05 06:51:00 +0300

    Add files via upload

---
+++ script-gallery.js
@@ -0,0 +1,53 @@
+const themeBtn = document.getElementById('themebtn');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+    body.classList.add('dark-mode');
+}
+
+themeBtn.addEventListener('click', () => {
+   
+    body.classList.toggle('dark-mode');
+   
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+window.onload = function() {
+    const lightbox = document.getElementById('lightbox');
+    const lbImg = document.getElementById('lbImg');
+    const lbTitle = document.getElementById('lbTitle');
+    const closeBtn = document.getElementById('lbClose');
+
+   
+    const links = document.querySelectorAll('.gallery-item a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); 
+
+            
+            const source = this.getAttribute('href');
+           
+            const text = this.parentElement.querySelector('.desc').innerHTML;
+
+          
+            lbImg.src = source;
+            lbTitle.innerHTML = text;
+
+           
+            lightbox.style.display = 'flex';
+        });
+    });
+
+    closeBtn.onclick = function() {
+        lightbox.style.display = 'none';
+    };
+};
+function goToSearch() {
+    const query = document.getElementById('homeSearchInput').value;
+    window.location.href = `search.html?q=${encodeURIComponent(query)}`;
+}
