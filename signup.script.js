bbed29a4b42b75af0b222a2a0db4f3ee3222268f
judy <judyessam230@gmail.com>
2026-05-09 14:19:48 +0300

    Update signup.script.js

--- signup.script.js
+++ signup.script.js
@@ -1,10 +1,47 @@
-function goToHome(event) {
-    event.preventDefault();
-    
+function goToHome() {
+
     document.body.classList.add('fade-out');
 
-  
-    setTimeout(function() {
-        window.location.href = "index.html"; 
-    }, 500); 
+    setTimeout(function () {
+        window.location.href = "index.html";
+    }, 500);
+}
+
+function validateSignup(event) {
+
+    event.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    
+    if (!email.includes("@")) {
+        alert("Email must contain @");
+        return;
+    }
+
+    
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters");
+        return;
+    }
+
+    
+    if (!/\d/.test(password)) {
+        alert("Password must contain at least one number");
+        return;
+    }
+
+    
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+   
+    localStorage.setItem("isLoggedIn", "true");
+
+   
+    goToHome();
 }
